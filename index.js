3b4fd4da71e28fcfac8313a2b77945917cb6670b
Caio Santos <caiosantosfreitas@hotmail.com>
2020-03-21 15:12:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -24,6 +24,33 @@ app.get('/alunos', function (req, res) {
     });
 })
 
+app.get('/genero', function (req, res) {
+    let total = 0
+    let masculino = 0
+    let feminino = 0
+    let outros = 0
+
+    fs.createReadStream('data.csv')
+        .pipe(csv())
+        .on('data', function(linha) {
+
+            if (linha["6 - Qual o seu gênero?"].toLowerCase() == "masculino") masculino += 1
+            else if (linha["6 - Qual o seu gênero?"].toLowerCase() == "feminino") feminino += 1
+            else outros += 1
+
+            total += 1
+
+        })
+        .on('end', function() {
+
+            feminino = (feminino * 100 / total).toFixed(2) + ' %'
+            masculino = (masculino * 100 / total).toFixed(2) + ' %'
+            outros = (outros * 100 / total).toFixed(2) + ' %'
+
+            return res.send({feminino, masculino, outros}).status(200)
+    });
+})
+
 app.listen(3000, function () {
   console.log('app listening on port 3000!')
 })
